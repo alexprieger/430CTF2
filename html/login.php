e875cdaf2fec8c0e8232ce88249e774455a848c2
Alex Prieger <prieger@usc.edu>
2023-04-05 11:47:01 -0700

    Added new database format, updated register.php and login.php

--- html/login.php
+++ html/login.php
@@ -2,11 +2,7 @@
 $servername = "localhost";
 $username = "server";
 $password = "pbN967bgWUAgdb5X3BmBxI2F";
-$dbname = "bank";
-
-file_put_contents('php://stderr', print_r("Logging in\n", TRUE));
-
-$numberOfDesiredBytes = 16;
+$dbname = "mydb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -21,32 +17,38 @@ $reg_password = $_GET['pass'];
 
 //to prevent from mysqli injection  
 $username = stripcslashes($reg_username);  
-$password = stripcslashes($reg_password);  
 $username = mysqli_real_escape_string($conn, $username);  
-$password = mysqli_real_escape_string($conn, $password);  
 
 // retrieving salt
-$retrieve_salt_sql = "select * from bank.users where username= '$username'";
-$salt_result = mysqli_query($conn, $retrieve_salt_sql);  
-$salt_row = mysqli_fetch_array($salt_result, MYSQLI_ASSOC);  
-$salt_count = mysqli_num_rows($salt_result);  
+$retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
+try {
+	$salt_result = $conn->query($retrieve_salt_sql);
+} catch (mysqli_sql_exception $e) {
+	die("Error logging in: " . $e->getMessage());
+}
+$salt_object = $salt_result->fetch_object();  
 
-
-if($salt_count == 1) {
-  $salt_value = $salt_row['salt'];
-  $hashed_password = hash("sha256", $password.$salt_value); 
+if($salt_object != null) {
+  $salt_value = $salt_object->salt; // Binary format
+  $hashed_password = md5($reg_password . $salt_value); 
+} else {
+	die("No user found with that username");
 }
 
-$cookie_value = random_bytes($numberOfDesiredBytes);
-$sql = "UPDATE users SET cookies = X'" . bin2hex($cookie_value) . "' WHERE username = '$username' and password = '$hashed_password'";
-$result = mysqli_query($conn, $sql);  
+$sql = "SELECT * FROM users WHERE username = '$username' AND hashed_password = '$hashed_password' LIMIT 1";
+try {
+	$result = mysqli_query($conn, $sql);
+} catch (mysqli_sql_exception $e) {
+	die("Error logging in: " . $e->getMessage());
+}
+$user_object = $result->fetch_object();
 
-if($conn->affected_rows == 1){ // Login succeeded, cookie is being set
-  setcookie("auth", $cookie_value, time() + (86400 * 30)); 
-  file_put_contents('php://stderr', print_r("Login successful, cookies set to " . bin2hex($cookie_value) . "\n", TRUE));
-} else{
-  file_put_contents('php://stderr', print_r("Login failed: " . $conn->error . "\n", TRUE));
-  echo "Login failed.";  
+if($user_object != null) { // Login succeeded
+	header("Location: success.html");
+  exit();
+} else { // Login failed, passwords did not match
+  header("Location: failure.html");
+  exit();  
 }
 
 ?>
