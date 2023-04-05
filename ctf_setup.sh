0c1d66178ee81ff32d6985a96edbe6f6b83f7c92
Alex Prieger <prieger@usc.edu>
2023-04-05 11:54:51 -0700

    Changed setup script to only create user if it doesn't already exist

--- ctf_setup.sh
+++ ctf_setup.sh
@@ -3,7 +3,7 @@ yes | sudo apt install php libapache2-mod-php php-mysql
 yes | sudo apt install mysql-server
 sudo cp html/* /var/www/html/
 # Create our user 'server'
-sudo mysql -e "CREATE USER 'server'@'localhost' IDENTIFIED BY 'pbN967bgWUAgdb5X3BmBxI2F';"
+sudo mysql -e "CREATE USER IF NOT EXISTS 'server'@'localhost' IDENTIFIED BY 'pbN967bgWUAgdb5X3BmBxI2F';"
 # Create an examle database and populate it with some initial values
 sudo mysql < sql-script.sql
 sudo mysql -e "GRANT ALL PRIVILEGES ON mydb.* TO 'server'@'localhost';"
