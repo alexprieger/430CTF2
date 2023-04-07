41e9b9b3fb2c45e761ffeedd6360de74ea2b58d4
Natalie Shamilian <natalieshamilian@Natalies-Air.lan>
2023-04-06 17:03:45 -0700

    debugging

--- html/email.php
+++ html/email.php
@@ -2,34 +2,34 @@
 
 alert("in email.php");
 // the link in the email should redirect to "reset.html"
-$servername = "localhost";
-$username = "server";
-$password = "pbN967bgWUAgdb5X3BmBxI2F";
-$dbname = "mydb";
+// $servername = "localhost";
+// $username = "server";
+// $password = "pbN967bgWUAgdb5X3BmBxI2F";
+// $dbname = "mydb";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
-// getting username and password from the url
-$reg_username = $_GET['user'];
-alert($reg_username);
+// // getting username and password from the url
+// $reg_username = $_GET['user'];
+// alert($reg_username);
 
-$username = stripcslashes($reg_username);  
+// $username = stripcslashes($reg_username);  
 
-$username = mysqli_real_escape_string($conn, $username); 
+// $username = mysqli_real_escape_string($conn, $username); 
 
-// retrieving salt
-$retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
-try {
-	$salt_result = $conn->query($retrieve_salt_sql);
-} catch (mysqli_sql_exception $e) {
-	die("Error finding user in: " . $e->getMessage());
-}
-$salt_object = $salt_result->fetch_object();  
+// // retrieving salt
+// $retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
+// try {
+// 	$salt_result = $conn->query($retrieve_salt_sql);
+// } catch (mysqli_sql_exception $e) {
+// 	die("Error finding user in: " . $e->getMessage());
+// }
+// $salt_object = $salt_result->fetch_object();  
 header("Location: success.html");
 exit();
 // if($salt_object != null) {
