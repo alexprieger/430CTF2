f367f3d006ab8b035ea7dbab01763c4d5ceeb120
Natalie Shamilian <natalieshamilian@Natalies-Air.lan>
2023-04-06 17:50:23 -0700

    debug

--- html/forgot.php
+++ html/forgot.php
@@ -13,7 +13,6 @@ if ($conn->connect_error) {
 
 // getting username and password from the url
 $reg_username = $_GET['user'];
-$reg_password = $_GET['pass'];
 
 //to prevent from mysqli injection  
 $username = stripcslashes($reg_username);  
@@ -29,26 +28,11 @@ try {
 $salt_object = $salt_result->fetch_object();  
 
 if($salt_object != null) {
-  $salt_value = $salt_object->salt; // Binary format
-  $hashed_password = md5($reg_password . $salt_value); 
-} else {
-	die("No user found with that username");
-}
-
-$sql = "SELECT * FROM users WHERE username = '$username' AND hashed_password = '$hashed_password' LIMIT 1";
-try {
-	$result = mysqli_query($conn, $sql);
-} catch (mysqli_sql_exception $e) {
-	die("Error logging in: " . $e->getMessage());
-}
-$user_object = $result->fetch_object();
-
-if($user_object != null) { // Login succeeded
-	header("Location: success.html");
+  header("Location: success.html");
   exit();
-} else { // Login failed, passwords did not match
-  header("Location: failure.html");
-  exit();  
+} else {
+	header("Location: failure.html");
+  exit(); 
 }
 
 ?>
