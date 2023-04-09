c8625b57369881180e97321f3f3c255aeb1da9e0
Desiree Khoo <dhkhoo@usc.edu>
2023-04-09 14:10:04 -0700

    commented out some echos

--- html/register.php
+++ html/register.php
@@ -23,7 +23,7 @@ if(sizeof($split_pass) < 8) { // check if there are at least 8 words
 	return;
 }
 
-echo "made it pass the first check";
+//echo "made it pass the first check";
 
 foreach($split_pass as $word) {
 	if (!ctype_lower($word)) { // check if everything is lowercase
@@ -32,31 +32,32 @@ foreach($split_pass as $word) {
 	}
 }
 
-echo "made it pass the second check";
+//echo "made it pass the second check";
 // check if all words are in the dictionary
 try{
 	$file = fopen("dictionary.txt", "r");
-	for($i=0; $i < sizeof($split_pass); $i++) {
-		$found = false;
-		while(!feof($file)) {
-			$line = fgets($file);
-			if(trim($line) == trim($split_pass[$i])) {
-				echo $line . " matches " . $split_pass[$i];
-				$found = true;
-				rewind($file);
-				break;
-			}
-		}
-		if(!$found) {
-			echo "Password only have valid dictionary words: ". $split_pass[$i]." is not a valid word";
-			return;
-		}
-	}
-	$fclose($file);
 } catch(Exception $e) {
 	echo "Error: " . $e->getMessage();
 }
 
+for($i=0; $i < sizeof($split_pass); $i++) {
+	$found = false;
+	while(!feof($file)) {
+		$line = fgets($file);
+		if(trim($line) == trim($split_pass[$i])) {
+			//echo $line . " matches " . $split_pass[$i];
+			$found = true;
+			rewind($file);
+			break;
+		}
+	}
+	if(!$found) {
+		echo "Password only have valid dictionary words: ". $split_pass[$i]." is not a valid word";
+		return;
+	}
+}
+
+$fclose($file);
 
 echo "made it pass the third check";
 //echo "Password is valid.";
