ea20eed4f797ffb62ceebe05c6f27b13b4f2d28a
Lydia DiBlasio <diblasio@usc.edu>
2023-04-10 12:28:06 -0700

    alter shell exec command for email sending

--- html/forgot.php
+++ html/forgot.php
@@ -22,7 +22,6 @@ $username = mysqli_real_escape_string($conn, $username);
 
 // retrieving salt
 $retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
-echo "retrieved salt ";
 try {
 	$salt_result = $conn->query($retrieve_salt_sql);
 } catch (mysqli_sql_exception $e) {
@@ -31,13 +30,18 @@ try {
 $salt_object = $salt_result->fetch_object();  
 
 if($salt_object != null) {
-  echo "made it inside forgot.php emailing";
   $email = $username + "@usc.edu";
   $reset = random_bytes($numberOfDesiredBytes);
   $sql_reset = "UPDATE users SET reset = '$reset' WHERE username= '$username'";
 
   $reset_link = "http://3.133.129.167/reset.html?reset=$reset";
-  exec("echo \"Reset your password here: $reset_link\" | mail -s \"CTF Team 4 Reset Password\" $email");
+  $shell_cmd = "echo "Reset your password here: $reset_link" | mail -s "CTF Team 4 Reset Password" $email";
+  // exec("echo \"Reset your password here: $reset_link\" | mail -s \"CTF Team 4 Reset Password\" $email");
+  $output = shell_exec($shell_cmd);
+  echo $output;
+  $output2 = shell_exec("bash ./mail.sh $reset_link $email");
+  echo $output2;
+  echo "Email sent to $email";
   header("Location: success.html");
   exit();
  } else {
