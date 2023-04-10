65235e254380997be71570f2645409dce40984e4
Scott Susanto <scottsus@usc.edu>
2023-04-10 16:34:59 -0700

    add metrics steps 1-5

--- html/forgot.php
+++ html/forgot.php
@@ -14,24 +14,24 @@ if ($conn->connect_error) {
 // getting username and password from the url
 $reg_username = $_GET['user'];
 
-//to prevent from mysqli injection  
-$username = stripcslashes($reg_username);  
-$username = mysqli_real_escape_string($conn, $username);  
+//to prevent from mysqli injection
+$username = stripcslashes($reg_username);
+$username = mysqli_real_escape_string($conn, $username);
 
 // retrieving salt
 $retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
 try {
-	$salt_result = $conn->query($retrieve_salt_sql);
+  $salt_result = $conn->query($retrieve_salt_sql);
 } catch (mysqli_sql_exception $e) {
-	die("Error logging in: " . $e->getMessage());
+  die("Error logging in: " . $e->getMessage());
 }
-$salt_object = $salt_result->fetch_object();  
+$salt_object = $salt_result->fetch_object();
 
 if($salt_object != null) {
   $email = $username . "@usc.edu";
   // echo " email is: " . $email;
   $reset = random_bytes($numberOfDesiredBytes);
-  $reset = bin2hex($reset);
+  $reset_hex = bin2hex($reset);
   // echo " reset is: " . $reset;
 
   $sql_reset = "UPDATE users SET reset = '$reset' WHERE username= '$username'";
@@ -41,16 +41,39 @@ if($salt_object != null) {
     die("Error updating reset: " . $e->getMessage());
   }
 
-  $reset_link = "3.133.129.167/reset.html?reset=$reset";
+  $reset_link = "3.133.129.167/reset.html?reset=$reset_hex";
   // echo "\nReset link is: " . $reset_link;
 
   $output = shell_exec("bash ./mail.sh $reset_link $email");
 
   header("Location: success.html");
-  exit();
+
+  $password_file = fopen("password_stats.txt", "r");
+  $password_resets = 0;
+
+  while (($line = fgets($password_file)) !== false)
+  {
+    $parts = explode(":", $line);
+    if (count($parts) != 2)
+      continue;
+
+    $count = intval(trim($parts[1]));
+    switch (trim($parts[0]))
+    {
+      case "R":
+        $password_resets = $count;
+        break;
+    }
+  }
+
+  $password_resets++;
+  $updated_file_contents = "R: $password_resets\n";
+  file_put_contents("password_stats.txt", $updated_file_contents);
+
  } else {
-         header("Location: failure.html");
-   exit(); 
+  header("Location: failure.html");
  }
+
+ exit();
 
 ?>
--- html/login.php
+++ html/login.php
@@ -15,40 +15,71 @@ if ($conn->connect_error) {
 $reg_username = $_GET['user'];
 $reg_password = $_GET['pass'];
 
-//to prevent from mysqli injection  
-$username = stripcslashes($reg_username);  
-$username = mysqli_real_escape_string($conn, $username);  
+//to prevent from mysqli injection
+$username = stripcslashes($reg_username);
+$username = mysqli_real_escape_string($conn, $username);
 
 // retrieving salt
 $retrieve_salt_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
 try {
-	$salt_result = $conn->query($retrieve_salt_sql);
+  $salt_result = $conn->query($retrieve_salt_sql);
 } catch (mysqli_sql_exception $e) {
-	die("Error logging in: " . $e->getMessage());
+  die("Error logging in: " . $e->getMessage());
 }
-$salt_object = $salt_result->fetch_object();  
+$salt_object = $salt_result->fetch_object();
 
 if($salt_object != null) {
   $salt_value = $salt_object->salt; // Binary format
-  $hashed_password = md5($reg_password . $salt_value); 
+  $hashed_password = md5($reg_password . $salt_value);
 } else {
-	die("No user found with that username");
+  die("No user found with that username");
 }
 
 $sql = "SELECT * FROM users WHERE username = '$username' AND hashed_password = '$hashed_password' LIMIT 1";
 try {
-	$result = mysqli_query($conn, $sql);
+  $result = mysqli_query($conn, $sql);
 } catch (mysqli_sql_exception $e) {
-	die("Error logging in: " . $e->getMessage());
+  die("Error logging in: " . $e->getMessage());
 }
 $user_object = $result->fetch_object();
 
-if($user_object != null) { // Login succeeded
-	header("Location: success.html");
-  exit();
-} else { // Login failed, passwords did not match
-  header("Location: failure.html");
-  exit();  
+$login_file = fopen("login_stats.txt", "r");
+$successful_logins = 0;
+$failed_logins = 0;
+$total_logins = 0;
+
+while (($line = fgets($login_file)) !== false)
+{
+  $parts = explode(":", $line);
+  if (count($parts) != 2)
+    continue;
+
+  $count = intval(trim($parts[1]));
+  switch (trim($parts[0]))
+  {
+    case "S":
+      $successful_logins = $count;
+      break;
+    case "F":
+      $failed_logins = $count;
+      break;
+    case "L":
+      $total_logins = $count;
+      break;
+  }
 }
 
+if($user_object != null) { // Login succeeded
+  $successful_logins++;
+  header("Location: success.html");
+} else { // Login failed, passwords did not match
+  $failed_logins++;
+  header("Location: failure.html");
+}
+
+$total_logins = $successful_logins + $failed_logins;
+$updated_file_contents = "S: $successful_logins\nF: $failed_logins\nL: $total_logins\n";
+file_put_contents("login_stats.txt", $updated_file_contents);
+
+exit();
 ?>
